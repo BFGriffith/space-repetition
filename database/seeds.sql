7f53d15cabd611ea170f9994ec80fc556e9137d0
Ednas <edna@edna.tech>
2016-07-11 21:13:44 -0400

    Dat Database tho h

--- database/seeds.sql
+++ database/seeds.sql
@@ -1,1 +1,35 @@
 //dummy-data inserts:
+INSERT INTO students (studentName)
+VALUES ('Pedro Sivero');
+
+INSERT INTO students (studentName)
+VALUES ('John Dispenette');
+
+INSERT INTO students (studentName)
+VALUES ('Ben Griffith');
+
+INSERT INTO students (studentName)
+VALUES ('Edna Neal');
+
+INSERT INTO students (studentName)
+VALUES ('Mark Dao');
+
+INSERT INTO students (studentName)
+VALUES ('Michael');
+
+INSERT INTO students (studentName)
+VALUES ('Victor');
+
+INSERT INTO students (studentName)
+VALUES ('Sean');
+
+INSERT INTO students (studentName)
+VALUES ('Eric');
+
+INSERT INTO students (studentName)
+VALUES ('Kenny');
+
+INSERT INTO subjects (subject, studentID)
+VALUES ('Kenny', 3);
+
+
