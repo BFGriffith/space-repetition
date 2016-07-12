1da8ae4f7eb1cd30523cf443c846646df6458958
Ednas <edna@edna.tech>
2016-07-11 20:46:20 -0400

    Database schema has been better-fied

--- database/schema.sql
+++ database/schema.sql
@@ -1,24 +1,55 @@
-CREATE DATABASE SpaceRepetition_db;
-USE SpaceRepetition_db;
+CREATE DATABASE SpacedRepetition_db;
+USE SpacedRepetition_db;
 
-CREATE TABLE student
+CREATE TABLE students
 (
-	id int NOT NULL AUTO_INCREMENT,
-	studentName varchar(255) NOT NULL,
-	PRIMARY KEY (id)
+    studentID int NOT NULL AUTO_INCREMENT,
+    studentName varchar(255) NOT NULL,
+    PRIMARY KEY (studentID)
 );
 
-CREATE TABLE studySubject
+CREATE TABLE subjects
 (
-	id int NOT NULL AUTO_INCREMENT,
-	ssubject varchar(255) NOT NULL,
-	PRIMARY KEY (id)	
+    subjectID int NOT NULL AUTO_INCREMENT,
+    subject varchar(255) NOT NULL,
+	PRIMARY KEY (subjectID),
+    studentID INT NOT NULL
 );
+
+ALTER TABLE subjects
+    ADD FOREIGN KEY (studentID) REFERENCES students(studentID);
+
+CREATE TABLE decks
+(
+    deckID int NOT NULL AUTO_INCREMENT,
+    deckName varchar(255) NOT NULL,
+    studentID INT NOT NULL,
+    subjectID INT NOT NULL,
+    PRIMARY KEY (deckID)
+);
+
+ALTER TABLE decks
+	ADD FOREIGN KEY (studentID) REFERENCES students(studentID);
+    
+ALTER TABLE decks
+    ADD FOREIGN KEY (subjectID) REFERENCES subjects(subjectID);
 
 CREATE TABLE cards
 (
-	id int NOT NULL AUTO_INCREMENT,
-	front varchar(255) NOT NULL,
-    back varchar(255),
-	PRIMARY KEY (id)	
+    cardID int NOT NULL AUTO_INCREMENT,
+    front varchar(1000) NOT NULL,
+    back varchar(1000) NOT NULL,
+    studentID INT NOT NULL,
+    subjectID INT NOT NULL,
+	deckID INT NOT NULL,
+    PRIMARY KEY (cardID)
 );
+
+ALTER TABLE cards
+    ADD FOREIGN KEY (studentID) REFERENCES students(studentID);
+    
+ALTER TABLE cards
+    ADD FOREIGN KEY (subjectID) REFERENCES subjects(subjectID);
+    
+ALTER TABLE cards
+    ADD FOREIGN KEY (deckID) REFERENCES decks(deckID);
