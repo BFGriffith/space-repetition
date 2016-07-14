9511e58453b30a37ed73107a5f48f159d99ca0c1
Ednas <edna@edna.tech>
2016-07-13 20:31:37 -0400

    cleaned up dashboard code

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var app = express();
-var expressHandlebars = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var connection = require('./configuration/connection.js');
 var path = require('path');
 
@@ -19,7 +19,7 @@ app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
-
+app.use(express.static(process.cwd() + '/assets'));
 require('./controllers/SpaceRepetitionController.js')(app);
 
 var port = 3000;
