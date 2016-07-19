a8b726e4fa001acff1332e72eeeb7e9bdcdabcdc
Ednas <edna@edna.tech>
2016-07-18 20:52:07 -0400

    added data to cards and removed duplicates

--- database/seeds.sql
+++ database/seeds.sql
@@ -1,4 +1,6 @@
-//dummy-data inserts:
+-- dummy-data inserts:
+
+----STUDENTS----
 INSERT INTO students (studentName)
 VALUES ('Pedro');
 
@@ -28,7 +30,8 @@ VALUES ('Eric');
 
 INSERT INTO students (studentName)
 VALUES ('Caroline');
---
+
+----SUBJECTS----
 
 INSERT INTO subjects (subject, studentID)
 VALUES ('CommandLine', 1);
@@ -52,6 +55,7 @@ INSERT INTO subjects (subject, studentID)
 VALUES ('JavaScript', 1);
 
 --
+
 INSERT INTO subjects (subject, studentID)
 VALUES ('HTML', 2);
 
@@ -74,6 +78,7 @@ INSERT INTO subjects (subject, studentID)
 VALUES ('CSS', 2);
 
 --
+
 INSERT INTO subjects (subject, studentID)
 VALUES ('HTML', 3);
 
@@ -94,6 +99,7 @@ VALUES ('JavaScript', 3);
 
 INSERT INTO subjects (subject, studentID)
 VALUES ('jQuery', 3);
+
 --
 
 INSERT INTO subjects (subject, studentID)
@@ -118,6 +124,7 @@ INSERT INTO subjects (subject, studentID)
 VALUES ('jQuery', 4);
 
 --
+
 INSERT INTO subjects (subject, studentID)
 VALUES ('Java', 5);
 
@@ -140,6 +147,7 @@ INSERT INTO subjects (subject, studentID)
 VALUES ('VersionControl', 5);
 
 --
+
 INSERT INTO subjects (subject, studentID)
 VALUES ('HTML', 6);
 
@@ -162,6 +170,7 @@ INSERT INTO subjects (subject, studentID)
 VALUES ('jQuery', 6);
 
 --
+
 INSERT INTO subjects (subject, studentID)
 VALUES ('Space Computers', 7);
 
@@ -182,6 +191,7 @@ VALUES ('Skylab', 7);
 
 INSERT INTO subjects (subject, studentID)
 VALUES ('Testing', 7);
+
 --
 
 INSERT INTO subjects (subject, studentID)
@@ -253,233 +263,14 @@ VALUES ('Firebase', 10);
 
 --
 
-INSERT INTO subjects (subject, studentID)
-VALUES ('CommandLine', 1);
+----DECKS----
 
-INSERT INTO subjects (subject, studentID)
-VALUES ('Git', 1);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('HTML', 1);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('CSS', 1);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('WebDesign', 1);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('DreamWeaver', 1);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('JavaScript', 1);
-
---
-INSERT INTO subjects (subject, studentID)
-VALUES ('HTML', 2);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('ComputerHistory', 2);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('InternetProtocols', 2);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('JavaScript', 2);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('jQuery', 2);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Node', 2);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('CSS', 2);
-
---
-INSERT INTO subjects (subject, studentID)
-VALUES ('HTML', 3);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('MySQL', 3);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('CSS', 3);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Handlebars', 3);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Express', 3);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('JavaScript', 3);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('jQuery', 3);
---
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('HTML', 4);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('CSS', 4);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Bootstrap', 4);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('FTP', 4);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('SQL', 4);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('JavaScript', 4);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('jQuery', 4);
-
---
-INSERT INTO subjects (subject, studentID)
-VALUES ('Java', 5);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('DataStructure', 5);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Testing', 5);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Compiler', 5);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Analysis', 5);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Design', 5);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('VersionControl', 5);
-
---
-INSERT INTO subjects (subject, studentID)
-VALUES ('HTML', 6);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('CSS', 6);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Bootstrap', 6);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('NPMpackets', 6);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Node.js', 6);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('JavaScript', 6);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('jQuery', 6);
-
---
-INSERT INTO subjects (subject, studentID)
-VALUES ('Space Computers', 7);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Rocket Computer Science', 7);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Hardware development', 7);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Arduino', 7);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Raspberry Pi', 7);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Skylab', 7);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Testing', 7);
---
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Firebase', 8);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('MySQL', 8);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Servers', 8);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('SASS', 8);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Git', 8);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('AJAX', 8);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('JSON', 8);
-
---
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Servers', 9);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Firebase', 9);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('FTP', 9);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('JSON', 9);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Express', 9);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Amber', 9);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Handlebars', 9);
-
---
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('jQuery', 10);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('ORM', 10);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('PHP', 10);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Java', 10);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Express', 10);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Handlebars', 10);
-
-INSERT INTO subjects (subject, studentID)
-VALUES ('Firebase', 10);
-
---
 INSERT INTO decks (deckName, StudentID, subjectID, CounterofAccess)
 values ('Commands', 1, 1, 1);
 
 INSERT INTO decks (deckName, StudentID, subjectID, CounterofAccess)
 values ('Tags', 1, 3, 1);
+
 --
 
 INSERT INTO decks (deckName, StudentID, subjectID, CounterofAccess)
@@ -487,6 +278,7 @@ values ('FTP', 2, 10, 1);
 
 INSERT INTO decks (deckName, StudentID, subjectID, CounterofAccess)
 values ('CSS Box Model', 2, 14, 1);
+
 --
 
 INSERT INTO decks (deckName, StudentID, subjectID, CounterofAccess)
@@ -494,6 +286,7 @@ values ('SQL Statements', 3, 16, 1);
 
 INSERT INTO decks (deckName, StudentID, subjectID, CounterofAccess)
 values ('CSS Box Model', 3, 17, 1);
+
 --
 
 INSERT INTO decks (deckName, StudentID, subjectID, CounterofAccess)
@@ -501,6 +294,7 @@ values ('Bootstrap does', 4, 24, 1);
 
 INSERT INTO decks (deckName, StudentID, subjectID, CounterofAccess)
 values ('jQuery functions', 4, 28, 1);
+
 --
 
 INSERT INTO decks (deckName, StudentID, subjectID, CounterofAccess)
@@ -508,6 +302,7 @@ values ('Objects', 5, 29, 1);
 
 INSERT INTO decks (deckName, StudentID, subjectID, CounterofAccess)
 values ('Jasmine', 5, 31, 1);
+
 --
 
 INSERT INTO decks (deckName, StudentID, subjectID, CounterofAccess)
@@ -515,6 +310,7 @@ values ('NPM commands', 6, 39, 1);
 
 INSERT INTO decks (deckName, StudentID, subjectID, CounterofAccess)
 values ('Node commands', 6, 40, 1);
+
 --
 
 INSERT INTO decks (deckName, StudentID, subjectID, CounterofAccess)
@@ -525,6 +321,7 @@ values ('Currents and Resistance', 7, 45, 1);
 
 INSERT INTO decks (deckName, StudentID, subjectID, CounterofAccess)
 values ('Margaret Hamilton', 7, 48, 1);
+
 --
 
 INSERT INTO decks (deckName, StudentID, subjectID, CounterofAccess)
@@ -532,6 +329,7 @@ values ('NoSQL', 8, 50, 1);
 
 INSERT INTO decks (deckName, StudentID, subjectID, CounterofAccess)
 values ('SQL Statements', 8, 51, 1);
+
 --
 
 INSERT INTO decks (deckName, StudentID, subjectID, CounterofAccess)
@@ -539,6 +337,7 @@ values ('NoSQL', 9, 58, 1);
 
 INSERT INTO decks (deckName, StudentID, subjectID, CounterofAccess)
 values ('Mustaches', 9, 63, 1);
+
 --
 
 INSERT INTO decks (deckName, StudentID, subjectID, CounterofAccess)
@@ -552,9 +351,9 @@ values ('Express syntax', 10, 68, 1);
 
 INSERT INTO decks (deckName, StudentID, subjectID, CounterofAccess)
 values ('NoSQL', 10, 70, 1);
---
 
------------Keep working from here---------------------
+----CARDS----
+
 INSERT INTO cards (front, back, StudentID, SubjectID, deckID)
 values ("ls", "lists all items in directory", 1, 1, 1);
 
@@ -566,5 +365,48 @@ values ("cd", "used to move into a different directory", 1, 1, 1);
 
 INSERT INTO cards (front, back, StudentID, SubjectID, deckID)
 values ("git add -A", "prepares your data to move to github", 1, 1, 1);
+
 --
 
+INSERT INTO cards (front, back, StudentID, SubjectID, deckID)
+values ("FTP", "Part of TCP/IP protocol suite - File Transfer protocol", 2, 10, 3);
+
+INSERT INTO cards (front, back, StudentID, SubjectID, deckID)
+values ("FTP port number", "21", 2, 10, 3);
+
+INSERT INTO cards (front, back, StudentID, SubjectID, deckID)
+values ("CSS Box Model", "The CSS Box Model. All HTML elements can be considered as boxes. In CSS, the term "box model" is used when talking about design and layout. The CSS box model is essentially a box that wraps around HTML elements, and it consists of: margins, borders, padding, and the actual content.", 2, 14, 4);
+
+INSERT INTO cards (front, back, StudentID, SubjectID, deckID)
+values ("Padding in HTML", "The padding clears an area around the content (inside the border) of an element. The padding is affected by the background color of the element. The top, right, bottom, and left padding can be changed independently using separate properties. A shorthand padding property can also be used, to change all paddings at once.", 2, 14, 4);
+
+--
+
+INSERT INTO cards (front, back, StudentID, SubjectID, deckID)
+values ("HTML", "Hyper Text Markup Language", 3, 16, 5);
+
+INSERT INTO cards (front, back, StudentID, SubjectID, deckID)
+values ("h1", "header tag, makes text large", 3, 16, 5);
+
+INSERT INTO cards (front, back, StudentID, SubjectID, deckID)
+values ("Padding", "Is inside a border and clears an area", 3, 17, 5);
+
+INSERT INTO cards (front, back, StudentID, SubjectID, deckID)
+values ("The box", "has a height and a width", 3, 17, 5);
+
+--
+
+INSERT INTO cards (front, back, StudentID, SubjectID, deckID)
+values ("Bootstrap is", "Bootstrap is the most popular HTML, CSS, and JavaScript framework for developing responsive, mobile-first web sites.", 4, 24, 5);
+
+INSERT INTO cards (front, back, StudentID, SubjectID, deckID)
+values ("Bootstrap grid system", "has 12 columns", 4, 24, 5);
+
+INSERT INTO cards (front, back, StudentID, SubjectID, deckID)
+values (".append() function", "Insert content, specified by the parameter, to the end of each element in the set of matched elements.", 4, 28, 6);
+
+INSERT INTO cards (front, back, StudentID, SubjectID, deckID)
+values (".ready() function", "Specify a function to execute when the DOM is fully loaded.", 4, 28, 6);
+
+
+-----------Keep working from here---------------------
