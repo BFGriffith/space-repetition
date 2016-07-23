d9e07c29b655436f59fb8696586e49bed794bd53
BFGriffith <benjaminforbesgriffith@gmail.com>
2016-07-23 02:29:52 -0400

    attempting jawsDB hotfix

--- server.js
+++ server.js
@@ -11,8 +11,15 @@ var PORT = 8080 || process.env;
 //Heroku + jawsDB
 var mysql = require('mysql');
 var connection = mysql.createConnection(process.env.JAWSDB_URL);
-
 connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
 
 //Handlebars-------------------------------------------------------
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
